78bcfabf2389499bc46fab8ad11b2d5d45536956
Lando Toothman <25360446+TheLandolorien@users.noreply.github.com>
2022-12-02 13:00:45 -0500

    refact: Store puzzle input alongside solution

--- src/advent_of_code/solver.py
+++ src/advent_of_code/solver.py
@@ -1,19 +1,25 @@
 import importlib
+import os
 import sys
-
-import advent_of_code.utilities.puzzle_input_reader as puzzle_input_reader
+import typing
 
 
 def solve():
     raise_for_invalid_arguments()
 
     puzzle_solver = importlib.import_module(f"advent_of_code.{(puzzle_name := sys.argv[1])}")
-    puzzle_solution = puzzle_solver.solve(puzzle_input=puzzle_input_reader.read(puzzle_name=puzzle_name))
+    puzzle_solution = puzzle_solver.solve(puzzle_input=read_puzzle_input(name=puzzle_name))
 
     print(title := f"{puzzle_name.title().replace('_', ' ')}")
     print("".join(["="] * len(title)))
     print(f"Part 1 - {puzzle_solution.first}")
     print(f"Part 2 - {puzzle_solution.second}")
+
+
+def read_puzzle_input(name: str) -> typing.List[str]:
+    puzzle_input_path = os.path.join(os.path.dirname(__file__), f"{name}.txt")
+    with open(puzzle_input_path) as puzzle_input:
+        return puzzle_input.readlines()
 
 
 def raise_for_invalid_arguments() -> None:
