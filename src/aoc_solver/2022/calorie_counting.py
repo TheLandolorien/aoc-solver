2c2344212e55ff6576a272aef4a66b8ec15ba82b
Lando Toothman <25360446+TheLandolorien@users.noreply.github.com>
2022-12-04 23:54:47 -0500

    refact: Separate calorie counter logic

--- src/aoc_solver/2022/calorie_counting.py
+++ src/aoc_solver/2022/calorie_counting.py
@@ -6,19 +6,25 @@ from aoc_solver.utilities import Solution
 # Source: https://adventofcode.com/2022/day/1
 
 
-def find_max_carried_calories(puzzle_input: typing.List[str], top: int = 1) -> int:
-    calories_carried_by_each_elf = [sum(map(int, calorie_counts.split(","))) for calorie_counts in ",".join(puzzle_input).split(",,")]
+def solve(puzzle_input=typing.List[str]) -> Solution:
+    carried_calories_per_elf = sum_carried_calories_per_elf(puzzle_input=puzzle_input)
 
+    return Solution(
+        first=sum_top_carried_calories(carried_calories=carried_calories_per_elf),
+        second=sum_top_carried_calories(carried_calories=carried_calories_per_elf, top=3),
+    )
+
+
+def sum_carried_calories_per_elf(puzzle_input=typing.List[str]) -> typing.List[int]:
+    grouped_calories = ",".join(puzzle_input).split(",,")
+    return [sum(map(int, elf_calories.split(","))) for elf_calories in grouped_calories]
+
+
+def sum_top_carried_calories(carried_calories: typing.List[int], top: int = 1) -> int:
     total_max_calories_carried = 0
+    calories_buffer = carried_calories[:]
     for _ in range(top):
-        total_max_calories_carried += (max_calories_carried := max(calories_carried_by_each_elf))
-        calories_carried_by_each_elf.remove(max_calories_carried)
+        total_max_calories_carried += (max_calories_carried := max(calories_buffer))
+        calories_buffer.remove(max_calories_carried)
 
     return total_max_calories_carried
-
-
-def solve(puzzle_input=typing.List[str]) -> Solution:
-    return Solution(
-        first=find_max_carried_calories(puzzle_input=puzzle_input),
-        second=find_max_carried_calories(puzzle_input=puzzle_input, top=3),
-    )
--- tests/aoc_solver/2022/test_calorie_counting.py
+++ tests/aoc_solver/2022/test_calorie_counting.py
@@ -1,13 +1,34 @@
+import pytest
 from aoc_solver.utilities import Solution
-
-
-def test_find_max_carried_calories_sum_top_max(puzzle_module, mock_puzzle_input):
-    assert puzzle_module.find_max_carried_calories(puzzle_input=mock_puzzle_input) == 24000
-
-
-def test_find_max_carried_calories_sums_multiple_top_maxes(puzzle_module, mock_puzzle_input):
-    assert puzzle_module.find_max_carried_calories(puzzle_input=mock_puzzle_input, top=3) == 45000
 
 
 def test_solve_calculates_puzzle_answers(puzzle_module, mock_puzzle_input):
     assert puzzle_module.solve(puzzle_input=mock_puzzle_input) == Solution(first=24000, second=45000)
+
+
+@pytest.mark.parametrize(
+    "puzzle_input,expected_totals",
+    [
+        (["100", "", "100", "100", "", "100", "100", "100"], [100, 200, 300]),
+        (["50", "100", "", "200", "400", "1000", "", "2500", "", "300", "500"], [150, 1600, 2500, 800]),
+        (["50"], [50]),
+    ],
+)
+def test_sum_carried_calories_per_elf(puzzle_input, expected_totals, puzzle_module):
+    assert puzzle_module.sum_carried_calories_per_elf(puzzle_input=puzzle_input) == expected_totals
+
+
+@pytest.mark.parametrize(
+    "carried_calories,top,expected_total",
+    [
+        ([100, 200, 300], 1, 300),
+        ([100, 200, 300], 2, 500),
+        ([150, 1600, 2500, 800], 4, 5050),
+    ],
+)
+def test_sum_top_carried_calories(carried_calories, top, expected_total, puzzle_module):
+    assert puzzle_module.sum_top_carried_calories(carried_calories=carried_calories, top=top) == expected_total
+
+
+def test_sum_top_carried_calories_gets_single_max_by_default(puzzle_module):
+    assert puzzle_module.sum_top_carried_calories(carried_calories=[150, 1600, 2500, 800]) == 2500
