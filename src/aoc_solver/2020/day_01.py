bb307f80980193e88421525a61de71d2cfce5887
Lando Toothman <25360446+TheLandolorien@users.noreply.github.com>
2023-12-03 22:18:34 -0500

    feat: Add 2020 Day 1 solutions

--- src/aoc_solver/2020/day_01.py
+++ src/aoc_solver/2020/day_01.py
@@ -1,4 +1,6 @@
+import math
 import typing
+from itertools import combinations
 
 from aoc_solver.object_types import Solution
 
@@ -6,16 +8,11 @@ from aoc_solver.object_types import Solution
 # Source: https://adventofcode.com/2020/day/1
 
 
-def find_product_of_2020_addends(puzzle_input: typing.List[str]) -> int:
-    first, second = find_2020_addends(expense_report=parse_puzzle(input=puzzle_input))
-    return first * second
-
-
-def find_2020_addends(expense_report: typing.List[int]) -> typing.Tuple[int]:
-    for entry in expense_report:
-        composite = 2020 - entry
-        if composite in expense_report:
-            return entry, composite
+def find_product_of_2020_addends(puzzle_input: typing.List[str], count: int) -> int:
+    expense_report = parse_puzzle(input=puzzle_input)
+    for addends in combinations(expense_report, count):
+        if sum(addends) == 2020:
+            return math.prod(addends)
 
 
 def parse_puzzle(input: typing.List[str]) -> typing.List[int]:
@@ -24,6 +21,6 @@ def parse_puzzle(input: typing.List[str]) -> typing.List[int]:
 
 def solve(puzzle_input: typing.List[str]) -> Solution:
     return Solution(
-        first=find_product_of_2020_addends(puzzle_input=puzzle_input),
-        second=None,
+        first=find_product_of_2020_addends(puzzle_input=puzzle_input, count=2),
+        second=find_product_of_2020_addends(puzzle_input=puzzle_input, count=3),
     )
--- tests/aoc_solver/2020/test_day_01.py
+++ tests/aoc_solver/2020/test_day_01.py
@@ -3,5 +3,5 @@ from aoc_solver.object_types import Solution
 
 def test_solve_calculates_puzzle_answers(puzzle_module, mock_puzzle_input):
     assert puzzle_module.solve(puzzle_input=mock_puzzle_input) == Solution(
-        first=514579, second=None
+        first=514579, second=241861950
     )
