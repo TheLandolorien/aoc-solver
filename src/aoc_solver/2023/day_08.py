7628fd422c473b08961f8898500535b57e18b344
Lando Toothman <25360446+TheLandolorien@users.noreply.github.com>
2023-12-10 17:50:23 -0500

    feat: Refactor 2023 Day 8 solutions

--- src/aoc_solver/2023/day_08.py
+++ src/aoc_solver/2023/day_08.py
@@ -1,53 +1,82 @@
 import typing
 from collections import namedtuple
+from math import lcm
 
 from aoc_solver.object_types import Solution
 
-Network = namedtuple("Network", ["instructions", "nodes"])
+Network = namedtuple("Network", ["instructions", "nodes", "starting_nodes"])
 
 # --- Day 8: Haunted Wasteland ---
 # Source: https://adventofcode.com/2023/day/8
 
 
-def count_steps(raw_instructions: typing.List[str], has_multi_paths: bool = False) -> int:
-    network = parse_input(raw_instructions=raw_instructions)
+def count_steps(raw_instructions: typing.List[str]) -> int:
+    network = parse_input(raw_instructions=raw_instructions, has_multi_paths=False)
 
-    current_nodes = (
-        [node for node in network.nodes.keys() if node.endswith("A")]
-        if has_multi_paths
-        else [list(network.nodes.keys())[0]]
-    )
     step = 0
-    num_instructions = len(network.instructions)
-    num_paths = len(current_nodes)
-    all_zs = all([node.endswith("Z") for node in current_nodes])
+    node = network.starting_nodes[0]
 
-    while not all_zs:
-        z_enders = 0
-        for idx, node in enumerate(current_nodes):
-            current_nodes[idx] = network.nodes[node][network.instructions[step % num_instructions]]
-            if current_nodes[idx].endswith("Z"):
-                z_enders += 1
-
-        all_zs = z_enders == num_paths
+    while not node.endswith("Z"):
+        node = network.nodes[node][network.instructions[step % len(network.instructions)]]
         step += 1
 
     return step
 
 
-def parse_input(raw_instructions: typing.List[str]) -> Network:
-    instructions = [1 if x == "R" else 0 for x in raw_instructions[0]]
+def calculate_steps(raw_instructions: typing.List[str]) -> int:
+    network = parse_input(raw_instructions=raw_instructions, has_multi_paths=True)
+
+    patterns = []
+    for node in network.starting_nodes:
+        pattern = calculate_z_ender_pattern(node=node, network=network)
+        patterns.append(pattern)
+
+    return lcm(*patterns)
+
+
+def calculate_z_ender_pattern(node: str, network: Network) -> int:
+    z_indices = []
+    current_node = node
+    i = 0
+
+    while len(z_indices) < 10:
+        current_node = network.nodes[current_node][
+            network.instructions[i % len(network.instructions)]
+        ]
+        if current_node.endswith("Z"):
+            z_indices.append(i)
+        i += 1
+
+    return calculate_pattern(sequence=z_indices)
+
+
+def calculate_pattern(sequence: typing.List[int]) -> int:
+    if len(set(sequence)) == 1:
+        return sequence[0]
+
+    return calculate_pattern(
+        sequence=[term - sequence[idx - 1] for idx, term in enumerate(sequence) if idx > 0],
+    )
+
+
+def parse_input(raw_instructions: typing.List[str], has_multi_paths: bool) -> Network:
     nodes = {}
+    starting_nodes = []
 
     for raw_node in raw_instructions[2:]:
         node, edges = raw_node.split(" = ")
-        nodes[node] = edges[1:-1].split(", ")
+        nodes[node] = dict(zip(["L", "R"], edges[1:-1].split(", ")))
+        if node.endswith("A"):
+            starting_nodes.append(node)
 
-    return Network(instructions=instructions, nodes=nodes)
+    if not has_multi_paths:
+        starting_nodes = starting_nodes[:1]
+
+    return Network(instructions=raw_instructions[0], nodes=nodes, starting_nodes=starting_nodes)
 
 
 def solve(puzzle_input: typing.List[str]) -> Solution:
     return Solution(
         first=count_steps(raw_instructions=puzzle_input),
-        second=count_steps(raw_instructions=puzzle_input, has_multi_paths=True),
+        second=calculate_steps(raw_instructions=puzzle_input),
     )
--- tests/aoc_solver/2023/test_day_08.py
+++ tests/aoc_solver/2023/test_day_08.py
@@ -7,13 +7,13 @@ from aoc_solver.object_types import Solution
 @pytest.fixture
 def mock_nodes() -> typing.Dict[str, typing.List[str]]:
     return {
-        "AAA": ["BBB", "CCC"],
-        "BBB": ["DDD", "EEE"],
-        "CCC": ["ZZZ", "GGG"],
-        "DDD": ["DDD", "DDD"],
-        "EEE": ["EEE", "EEE"],
-        "GGG": ["GGG", "GGG"],
-        "ZZZ": ["ZZZ", "ZZZ"],
+        "AAA": {"L": "BBB", "R": "CCC"},
+        "BBB": {"L": "DDD", "R": "EEE"},
+        "CCC": {"L": "ZZZ", "R": "GGG"},
+        "DDD": {"L": "DDD", "R": "DDD"},
+        "EEE": {"L": "EEE", "R": "EEE"},
+        "GGG": {"L": "GGG", "R": "GGG"},
+        "ZZZ": {"L": "ZZZ", "R": "ZZZ"},
     }
 
 
@@ -37,13 +37,13 @@ def test_solve_calculates_puzzle_answers(puzzle_module, mock_puzzle_input):
         "22Z = (22B, 22B)",
         "XXX = (XXX, XXX)",
     ]
-    assert puzzle_module.solve(puzzle_input=mock_puzzle_input) == Solution(first=2, second=2)
-    assert puzzle_module.solve(puzzle_input=mock_puzzle_input_2) == Solution(first=6, second=6)
+    assert puzzle_module.solve(puzzle_input=mock_puzzle_input).first == 2
+    assert puzzle_module.solve(puzzle_input=mock_puzzle_input_2).first == 6
     assert puzzle_module.solve(puzzle_input=mock_puzzle_input_3) == Solution(first=2, second=6)
 
 
 def test_parse_input(puzzle_module, mock_puzzle_input, mock_nodes):
-    network = puzzle_module.parse_input(raw_instructions=mock_puzzle_input)
+    network = puzzle_module.parse_input(raw_instructions=mock_puzzle_input, has_multi_paths=False)
 
-    assert network.instructions == [1, 0], "should parse network instructions"
+    assert network.instructions == "RL", "should parse network instructions"
     assert network.nodes == mock_nodes, "should parse network nodes"
